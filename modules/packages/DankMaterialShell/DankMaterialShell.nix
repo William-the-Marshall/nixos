fd27c4853fd8888705f648e586f9d1cdfc8f4104
William-the-Marshall <jdfye96@gmail.com>
2026-07-16 19:34:19 -0700

    dms final?

--- modules/packages/DankMaterialShell/DankMaterialShell.nix
+++ modules/packages/DankMaterialShell/DankMaterialShell.nix
@@ -1,20 +1,33 @@
 {inputs, ...}: {
 
-  flake-file.inputs ={
+  flake-file.inputs = {
     DankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell/stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   flake.nixosModules.DankMaterialShell = {pkgs, lib, ...}: {
+    imports = [ inputs.DankMaterialShell.nixosModules.dank-material-shell ];
+
+    programs.dank-material-shell = {
+      enable = true;
+
+      systemd = {
+        enable = true;           # autostart via systemd user service
+        restartIfChanged = true; # restart dms.service when the module changes
+      };
+
+      enableSystemMonitoring = true;  # dgop
+      enableDynamicTheming = true;    # matugen
+      enableAudioWavelength = true;   # cava
+    };
 
     environment.systemPackages = with pkgs; [
-      inputs.DankMaterialShell.packages.${pkgs.stdenv.hostPlatform.system}.default
       evtest
-      quickshell
       bemenu
     ];
-    
+
     services.tuned.enable = true;
     services.upower.enable = true;
 
