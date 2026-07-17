52222bcb2a6b13e86ac777bfeec50f6550a71253
William-the-Marshall <jdfye96@gmail.com>
2026-07-17 10:56:29 -0700

    dms binds

--- modules/packages/niri/niri.nix
+++ modules/packages/niri/niri.nix
@@ -36,6 +36,12 @@
       files = {
         ".config/niri/config.kdl".source = ./configs/config.kdl;
         ".config/niri/dms/binds.kdl".source = ./configs/dms/binds.kdl;
+        ".config/niri/dms/colors.kdl".source = ./configs/dms/colors.kdl;
+        ".config/niri/dms/outputs.kdl".source = ./configs/dms/outputs.kdl;
+        ".config/niri/dms/cursor.kdl".source = ./configs/dms/cursor.kdl;
+        ".config/niri/dms/windowrules.kdl".source = ./configs/dms/windowrules.kdl;
+        ".config/niri/dms/alttab.kdl".source = ./configs/dms/alttab.kdl;
+        ".config/niri/dms/layout.kdl".source = ./configs/dms/layout.kdl;
       };
     };
 
